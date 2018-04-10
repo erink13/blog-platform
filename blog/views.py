b81b7186317129525cbc512c46cc30f2445a2ec7
Erin Kocis <erink13@vt.edu>
2018-04-09 21:10:47 -0400

    Pagination: import Django paginator classes and modify the post_list view and create a template to display the paginator

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 #takes request as the only parameter
 #this parameter is required by all views
 def post_list(request):
-    posts = Post.published.all() #all posts with published status
-    return render(request, 'blog/post/list.html', {'posts': posts}) #render list of posts
+    object_list = Post.published.all() #all posts with published status
+    paginator = Paginator(object_list, 3) # instantiate paginator object with 3 posts in each page
+    page = request.GET.get('page') # get page get parameter to indicate curr page number
+    try:
+        posts = paginator.page(page) #obtain objects for desired page with page method
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+        #pass page number and retrieved objects to the template
+    return render(request, 'blog/post/list.html', {'page': page,'posts': posts}) #render list of posts
 
 #post detail - every post can be identified by date and slug
 def post_detail(request, year, month, day, post):
